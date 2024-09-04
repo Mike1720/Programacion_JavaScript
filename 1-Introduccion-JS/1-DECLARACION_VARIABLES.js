b978592deabfa788eb112704ea92365f677f4343
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2024-09-03 20:24:47 -0600

    Introducción: Declaracion de variables

---
+++ 1-Introduccion-JS/1-DECLARACION_VARIABLES.js
@@ -0,0 +1,19 @@
+// * PRACTICA DECLARACION DE VARIABLES
+
+let numero = 15
+typeof numero // -> number
+
+let numeroFloat = 15.78
+typeof numeroFloat // -> number
+
+let booleano = true
+typeof booleano // -> boolean
+
+let string = "Caracteres"
+typeof string // -> string
+
+let vacio = null
+typeof vacio // -> object
+
+let indefinido;
+typeof indefinido // -> undefined
