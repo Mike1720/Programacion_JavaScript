dbffb7f77c98cf174fd2b4fccf40df7fc505fb23
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2024-09-03 20:51:55 -0600

    Funciones

---
+++ 1-Introduccion-JS/5-FUNCIONES.js
@@ -0,0 +1,22 @@
+// * FUNCIONES
+
+{
+  /* ESTRUCTURA DE UNA FUNCION
+    function -> palabra reservada
+    name -> Nombre de la función (Usar nombres descriptivos de la funcion ¿Que hace?)
+    (params) -> Parametros que recibe la función
+    {} -> Instrucciones que se ejecuta cuando se llama a la función 
+  */
+}
+
+function saludar (){
+  console.log("HOLA");
+}
+
+function suma() {
+  let result = 5 * 9
+  console.log(result);
+}
+
+saludar() // -> En terminal "HOLA"
+suma() // -> En terminal 45
