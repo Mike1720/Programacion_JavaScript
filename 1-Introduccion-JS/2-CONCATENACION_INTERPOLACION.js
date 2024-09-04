d5fdf3160321812562f876c57f5592c87b0695e4
Miguel Angel Miranda Velasco <mikemv1720@hotmail.com>
2024-09-03 20:39:09 -0600

    "Union de textos"

---
+++ 1-Introduccion-JS/2-CONCATENACION_INTERPOLACION.js
@@ -0,0 +1,11 @@
+// ***CONCATENACION E INTERPOLACION
+
+// *VARIABLES A UNIR
+let nombre = "Miguel"
+let apellido = "Miranda"
+
+// *CONCATENACIÓN
+console.log("Hola mi nombre es: " + nombre + " " + apellido);
+
+// *INTERPOLACION
+console.log(`Hola mi nombre es: ${nombre} ${apellido}`);
